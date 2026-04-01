13d8f3dfb4a03962006d306c586697a974d5cb78
Chleau <michelchloe04@gmail.com>
2026-04-01 16:32:37 +0200

    add textArea messageinutile

---
+++ Assets/02-Code/Message_inutile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Message_inutile : MonoBehaviour
+{
+    // Message qui sera affiché dans l'inspecteur
+    [TextArea]
+    public string monMessage = "Cet objet est totalement inutile...";
+}
