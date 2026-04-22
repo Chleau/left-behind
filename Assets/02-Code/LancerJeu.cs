63d4e4e1ba4ec616fb360f230fb0a38517bce7c6
Chleau <michelchloe04@gmail.com>
2026-04-22 15:39:56 +0200

    restart

--- Assets/02-Code/LancerJeu.cs
+++ Assets/02-Code/LancerJeu.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class LancerJeu : MonoBehaviour
 {
     public GameObject menuComplet;
+    public CompteARebours scriptChrono;
 
     void Start()
     {
-        // AU DÉBUT : On libère la souris pour pouvoir cliquer sur le menu
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
@@ -18,10 +19,24 @@ public class LancerJeu : MonoBehaviour
             menuComplet.SetActive(false);
         }
 
+        if (scriptChrono != null)
+        {
+            scriptChrono.LancerChrono();
+        }
+
         // QUAND ON JOUE : On verrouille la souris pour diriger le perso
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         
+        Time.timeScale = 1f;
+    }
+
+    public void Rejouer()
+    {
+        // Cette ligne recharge la scène active (celle où tu es)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+        // On s'assure que le temps reprend (au cas où il était en pause)
         Time.timeScale = 1f;
     }
 }
